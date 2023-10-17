42f0a0406b1c3700a95079a52e1a1ff2b9059e15
JamilVA <129433669+JamilVA@users.noreply.github.com>
2023-10-17 12:52:19 -0500

    VASGUDBSA

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,6 +1,6 @@
-const menuBtn = document.querySelector(".menu-btn")
+const menuBtn = document.querySelector(".menu-btn");
 
-
+const menu = document.querySelector(".menu");
 
 menuBtn.addEventListener("click", ()=>{
     menuBtn.classList.toggle("active"); 
@@ -9,8 +9,25 @@ menuBtn.addEventListener("click", ()=>{
     }else if(menuBtn.innerHTML=='<i class="bx bx-x" id="close-icon"></i>'){
         menuBtn.innerHTML='<i class="bx bx-menu" id="menu-icon"></i>';
     }
-    navigation.classList.toggle("active");
+    menu.classList.toggle("active");
 })
 
-const navigation = document.querySelector(".navigation")
 
+const dropdown = document.querySelectorAll(".dropdown");
+
+for(let i = 0; i< dropdown.length;i++){
+    dropdown[i].addEventListener("click",()=>{
+        dropdown[i].classList.toggle("active");
+    })
+};
+
+
+
+
+
+
+// menu.querySelectorAll(".dropdown").forEach((arrow => {
+//     arrow.addEventListener("click",()=>{
+//         this.closest(".dropdown").classList.toggle("active");
+//     })
+// }))
